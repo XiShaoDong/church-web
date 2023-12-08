77f46a66ef10067226e6c256dae260936f4d94ed
Yuxuan Wang <Eric.Wang@gordon.edu>
2023-12-08 12:33:40 -0500

    Load all files

---
+++ script.js
@@ -0,0 +1,23 @@
+let getBody =  document.querySelector('.transition_none');
+            
+            window.onload = function(){
+                console.log("test"+logo.item)
+            }
+
+
+
+// -----------------------------------------------------------
+var toggleBtn = document.getElementById('.toggler')
+const body = document.querySelector('body')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+logo.onclick = function() {
+    console.log("test!")
+}
+toggleBtn.onclick = function() {
+    // dropDownMenu.classList.toggle('open')
+    // const isOpen = dropDownMenu.classList.contains('open')
+    console.log("test")
+    console.log(body.style)
+    // toggleBtnIcon.classList = isOpen?'fa-solid fa-bars' : 'fa-solid fa-bars'
+
+}
